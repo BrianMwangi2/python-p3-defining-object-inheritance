62e04a3216b6874d1aca27b4ee926d6f09817be5
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-27 12:51:20 +0300

    passes

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,7 @@
 from vehicle import Vehicle
 
-class Car(Vehicle):
-    pass
+class Car(Vehicle): # vehicle (class) is added as an argument to the subclass Car so as to be inherited 
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+# We have inherited Car instances from the Vehicle class thus we don't need to write alot 
